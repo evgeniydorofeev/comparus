33f9f813a887caa7995558ec3858ed4693f1d8b4
evgeniydorofeev <evgeniy@oment.team>
2024-12-12 16:29:05 +0200

    update

--- src/main/java/comparus/UserController.java
+++ src/main/java/comparus/UserController.java
@@ -1,43 +1,19 @@
 package comparus;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import comparus.DataSourcesConfig.DataSourceConfig;
 
 @RestController
 public class UserController {
 
 	@Autowired
-	private DataSourcesConfig config;
+	private UserService userService;
 
 	@GetMapping("/users")
 	public List<UserDto> getUsers() {
-		List<UserDto> result = new ArrayList<>();
-		for (DataSourceConfig cfg : config.getDataSources()) {
-			result.addAll(getUsers(cfg));
-		}
-		return result;
-	}
-
-	private List<UserDto> getUsers(DataSourceConfig cfg) {
-		DataSource dataSource = DataSourceBuilder.create()
-				.url(cfg.getUrl())
-				.username(cfg.getUser())
-				.driverClassName(cfg.getDriverClassName())
-				.password(cfg.getPassword())
-				.build();
-		List<UserDto> users = new JdbcTemplate(dataSource).query(cfg.getSelectUserQuery(),
-				new BeanPropertyRowMapper<UserDto>(UserDto.class));
-		return users;
+		return userService.getUsers();
 	}
 }
---
+++ src/main/java/comparus/UserService.java
@@ -0,0 +1,47 @@
+package comparus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import comparus.DataSourcesConfig.DataSourceConfig;
+
+@Repository
+public class UserService {
+
+	@Autowired
+	private DataSourcesConfig config;
+
+	private Map<String, JdbcTemplate> jdbcTemplates = new ConcurrentHashMap<>();
+
+	public List<UserDto> getUsers() {
+		List<UserDto> users = new ArrayList<>();
+		for (DataSourceConfig cfg : config.getDataSources()) {
+			users.addAll(getUsers(cfg));
+		}
+		return users;
+	}
+
+	private List<UserDto> getUsers(DataSourceConfig cfg) {
+		JdbcTemplate jdbcTemplate = jdbcTemplates.get(cfg.getName());
+		if (jdbcTemplate == null) {
+			DataSource dataSource = DataSourceBuilder.create().url(cfg.getUrl()).username(cfg.getUser())
+					.driverClassName(cfg.getDriverClassName()).password(cfg.getPassword()).build();
+			jdbcTemplate = new JdbcTemplate(dataSource);
+			jdbcTemplates.put(cfg.getName(), jdbcTemplate);
+		}
+		;
+		List<UserDto> users = jdbcTemplate.query(cfg.getSelectUserQuery(),
+				new BeanPropertyRowMapper<UserDto>(UserDto.class));
+		return users;
+	}
+}
