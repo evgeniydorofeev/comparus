2a13127abfa880c005fd5c59e8355621ffea2d7d
evgeniydorofeev <evgeniy@oment.team>
2024-12-12 22:25:18 +0200

    update

--- src/test/java/com/comparus/IntegrationTest.java
+++ src/test/java/com/comparus/IntegrationTest.java
@@ -1,52 +1,55 @@
 package com.comparus;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Testcontainers
-@ContextConfiguration(initializers = {IntegrationTest.Initializer.class})
-@TestPropertySource(properties = "spring.config.location=classpath:/application-test.yml")
 public class IntegrationTest {
 
 	@Autowired
 	TestRestTemplate testRestTemplate;
+	
+	@DynamicPropertySource
+	static void registerPgJdbcUrl(DynamicPropertyRegistry registry) {
+		registry.add("data-sources[0].strategy", () -> "postgres");
+		registry.add("data-sources[0].user", () -> "postgres");
+		registry.add("data-sources[0].password", () -> "postgres");
+		registry.add("data-sources[0].table", () -> "users");
+		registry.add("data-sources[0].url", () -> postgres.getJdbcUrl());
+		registry.add("data-sources[0].mapping.id", () -> "user_id");
+		registry.add("data-sources[0].mapping.username", () -> "login");
+		registry.add("data-sources[0].mapping.name", () -> "first_name");
+		registry.add("data-sources[0].mapping.surname", () -> "last_name");
+	}
 
 	@Container
 	@SuppressWarnings("resource")
-	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17").withDatabaseName("postgres")
-			.withUsername("postgres").withPassword("postgres").withInitScript("init-db.sql").withExposedPorts(5432);
-
-	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-			TestPropertyValues.of("CONTAINER.URL=" + postgres.getJdbcUrl())
-					.applyTo(configurableApplicationContext.getEnvironment());
-		}
-	}
-
-	@DynamicPropertySource
-	static void registerPgProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url",
-				() -> String.format("jdbc:postgresql://localhost:%d/prop", postgres.getFirstMappedPort()));
-	}
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
+			.withDatabaseName("postgres")
+			.withUsername("postgres")
+			.withPassword("postgres")
+			.withInitScript("init-db.sql")
+			.withExposedPorts(5432);
 
 	@Test
-	void myTest() {
+	void test() {
 		UserDto[] dtos = testRestTemplate.getForObject("/users", UserDto[].class);
-		System.out.println(dtos);
+		assertEquals(1, dtos.length);
+		assertEquals("example-user-id-1", dtos[0].id);
+		assertEquals("user-1", dtos[0].username);
+		assertEquals("John", dtos[0].name);
+		assertEquals("Doe", dtos[0].surname);
 	}
 
 }
