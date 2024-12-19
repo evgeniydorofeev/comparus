270decd23e79c374c59ca04e2076f4e25fc63710
evgeniydorofeev <evgeniy@oment.team>
2024-12-19 08:33:25 +0200

    update

--- src/main/java/com/comparus/UserController.java
+++ src/main/java/com/comparus/UserController.java
@@ -3,20 +3,19 @@ package com.comparus;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @Tag(name="users", description = "User API")
 public class UserController {
-
-	@Autowired
-	private UserService userService;
+	private final UserService userService;
 
 	@GetMapping("/users")
 	@Parameter(name="users", description="Get users from all avaliable sources")
